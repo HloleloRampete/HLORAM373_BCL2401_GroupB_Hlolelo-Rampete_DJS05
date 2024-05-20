d0dc5ccb868787dcb1d104664a08ab98b98852c8
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-20 16:23:03 +0200

    Creating a Reducer Function

--- scripts.js
+++ scripts.js
@@ -4,3 +4,16 @@ const initialState = { count: 0 };
 const increment = () => ({ type: 'INCREMENT' });
 const decrement = () => ({ type: 'DECREMENT' });
 const reset = () => ({ type: 'RESET' });
+
+const tallyReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return { count: state.count + 1 };
+      case 'DECREMENT':
+        return { count: state.count - 1 };
+      case 'RESET':
+        return { count: 0 };
+      default:
+        return state;
+    }
+  };
