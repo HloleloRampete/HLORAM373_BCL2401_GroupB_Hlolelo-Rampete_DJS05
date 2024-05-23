af61decd582b9fb2d96e26d2fe844e517c6f8e93
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-23 13:14:50 +0200

    Removing web components

--- store.js
+++
@@ -1,7 +0,0 @@
-// create the store using the createStore function from the redux library
-import { createStore } from 'redux';
-import tallyReducer from '../reducers/tallyReducer';
-
-const store = createStore(tallyReducer);
-
-export default store;
